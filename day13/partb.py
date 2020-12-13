b2b003e562d432b82de620c36a2793d973364811
Jason Newman <jason@squarepenguin.com>
2020-12-13 12:34:17 -0700

    cleanup imports

--- day13/partb.py
+++ day13/partb.py
@@ -1,20 +1,16 @@
-import collections
-import math
-import re
-import sys
 from sympy.ntheory.modular import crt
 
-f = open('input.txt', 'r') 
+f = open('input.txt', 'r')
 lines = f.readlines()
 buses = lines[1].strip().split(',')
 
 mods = []
 values = []
 for i, v in enumerate(buses):
-  if v == 'x':
-    continue
-  values.append(int(v))
-  mods.append(-int(i))
+    if v == 'x':
+        continue
+    values.append(int(v))
+    mods.append(-int(i))
 
 results = crt(values, mods)
 
